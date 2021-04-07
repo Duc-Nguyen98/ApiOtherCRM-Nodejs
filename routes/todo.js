e197991695b4f98ee3a71de747cd6f83f95fbb5a
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-08 05:20:44 +0700

    Update todo.js
    Done API

--- routes/todo.js
+++ routes/todo.js
@@ -2,10 +2,39 @@ const express = require('express');
 const router = express.Router();
 const todoModel = require('../model/schemaTodo');
 
+
+const hasFilter = (task, keyword) => {
+  let isImportant = true;
+  let isDeleted = true;
+  let isCompleted = true;
+  if (task === 'important') {
+    return { isImportant: isImportant, isDeleted: !isDeleted, title: keyword }
+  } else if (task === 'completed') {
+    return { isCompleted: isCompleted, isDeleted: !isDeleted, title: keyword }
+  } else if (task === 'deleted') {
+    return { isDeleted: isDeleted, title: keyword }
+  } else {
+    return { isDeleted: !isDeleted, title: keyword }
+  }
+}
+
+const hasSort = type => {
+  if (type === 'title-asc') {
+    return { title: 1 }
+  } else if (type === "title-desc") {
+    return { title: -1 }
+  } else if (type === 'due-date-desc') {
+    return { dueDate: -1 }
+  } else {
+    return { dueDate: 1 }
+  }
+}
+
 //! CODE API FOR PERMISSION SUPER ADMIN - ADMIN
 /* GET home Todo listing. */
 // TODO: METHOD - GET
 // -u http://localhost:1509/todo/
+
 router.get('/', async function (req, res, next) {
   res.send({
     status: 200,
@@ -15,17 +44,29 @@ router.get('/', async function (req, res, next) {
 
 /* GET todo listing view MyTask */
 // TODO: METHOD - GET
-// -u http://localhost:1509/todo/task 
-router.get('/task', async function (req, res, next) {
+// -u http://localhost:1509/todo/task/:params?query(q=)&query(sort=)
+// ? Example : http://localhost:1509/todo/task/completed?q=5&sort=title-desc
+
+router.get('/task(/:task)?/', async function (req, res, next) {
   try {
+    let task = req.params.task;
+    let q = req.query.q;
+    let regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
+    let sort = req.query.sort;
+    let currentPage = parseInt(req.query.page);
+    console.log(currentPage)
     await todoModel
-      .find({ isDeleted: false })
+      .find(hasFilter(task, regex))
+      .sort(hasSort(sort))
+      .limit(10)
+      .skip((currentPage - 1) * 10)
       .then(data => {
         return res.status(200).json({
           success: true,
           data: data
-        });
+        })
       })
+
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -34,34 +75,27 @@ router.get('/task', async function (req, res, next) {
   };
 });
 
-/* GET todo listing view Important */
+/* GET todo listing return list todo follow Tags */
 // TODO: METHOD - GET
-// -u http://localhost:1509/todo/important
-router.get('/important', async function (req, res, next) {
+// -u http://localhost:1509/todo/task/tag/:params?query(q=)&query(sort=)
+// ? Example : http://localhost:1509/todo/task/tag/medium?q=c&sort=due-date-desc
+router.get('/task/tag(/:tags)?/', async function (req, res, next) {
   try {
-    await todoModel
-      .find({ isDeleted: false, isImportant: true })
-      .then(data => {
-        return res.status(200).json({
-          success: true,
-          data: data
-        });
-      })
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      error: 'Server Error'
-    });
-  };
-});
+    let q = req.query.q;
+    let regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
+    let sort = req.query.sort;
+    let tag = req.params.tags;
+    let currentPage = parseInt(req.query.page);
 
-/* GET todo listing view Deleted */
-// TODO: METHOD - GET
-// -u http://localhost:1509/todo/deleted
-router.get('/deleted', async function (req, res, next) {
-  try {
     await todoModel
-      .find({ isDeleted: true })
+      .find({
+        tags: tag,
+        isDeleted: false,
+        title: regex
+      })
+      .sort(hasSort(sort))
+      .limit(10)
+      .skip((currentPage - 1) * 10)
       .then(data => {
         return res.status(200).json({
           success: true,
@@ -79,6 +113,7 @@ router.get('/deleted', async function (req, res, next) {
 /* GET Details users listing. */
 // TODO: METHOD - GET
 // -u http://localhost:1509/todo/task/detail/:id
+
 router.get('/task/detail/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
@@ -101,6 +136,7 @@ router.get('/task/detail/:id', async function (req, res, next) {
 /* POST todo listing create a record. */
 // TODO: METHOD - POST
 // -u http://localhost:1509/todo/task/create
+
 router.post('/task/create', async function (req, res, next) {
   try {
     const entry = await todoModel.create({
@@ -130,6 +166,7 @@ router.post('/task/create', async function (req, res, next) {
 /* PUT todo listing. update an record */
 // TODO: METHOD - PUT
 // -u http://localhost:1509/todo/task/update/:id
+
 router.put('/task/update/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
@@ -161,6 +198,7 @@ router.put('/task/update/:id', async function (req, res, next) {
 /* PATCH todo listing change status isComplete. */
 // TODO: METHOD - PATCH
 // -u http://localhost:1509/todo/task/change/:id
+
 router.patch('/task/change/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
@@ -181,6 +219,7 @@ router.patch('/task/change/:id', async function (req, res, next) {
 /* PATCH todo listing deleteSoft Record */
 // TODO: METHOD - PATCH
 // -u http://localhost:1509/todo/task/delete/:id
+
 router.patch('/task/delete/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
@@ -200,6 +239,7 @@ router.patch('/task/delete/:id', async function (req, res, next) {
 /* DELETE todo listing deleteSoft Record */
 // TODO: METHOD - DELETE
 // -u http://localhost:1509/todo/task/delete/:id
+
 router.delete('/task/delete/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
@@ -216,6 +256,18 @@ router.delete('/task/delete/:id', async function (req, res, next) {
   };
 });
 
+
+/* GET todo listing Search Record */
+// TODO: METHOD - GET
+// -u http://localhost:1509/todo/task/search/?keyword=abc
+
+// router.get('/task/search', async function (req, res, next) {
+//   let q = req.query.q;
+//   var regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
+//   return todoModel.find({ title: regex }, function (err, q) {
+//     return res.send(q);
+//   });
+// });
 
 
 //! CODE API FOR PERMISSION EMPLOYEE
