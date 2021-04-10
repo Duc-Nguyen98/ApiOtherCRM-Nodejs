5470565260d1281cd88361af7bae25c4910201db
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-11 06:04:14 +0700

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mailModel = require('../model/schemaEMail');
+const { ObjectId } = require('mongodb');
+
+
+
 
 let isStarred = true;
 let isRead = true;
@@ -45,72 +49,6 @@ let handlePaLabels = (param2, param3) => {
   }
 }
 
-// let handleUpdateAnRecord = (param, pram2) => {
-//   switch (param) {
-//     case 'personal':
-//       return { labels: 'personal' }
-//       break;
-//     case 'company':
-//       return { labels: 'company' }
-//       break;
-//     case 'important':
-//       return { labels: 'important' }
-//       break;
-//     case 'private':
-//       return { labels: 'private' }
-//       break;
-
-//     case 'isStarred':
-//       return { labels: 'private' }
-//       break;
-
-//     case 'restore': //? restore soft
-//       return { isRead: isRead }
-//       break;
-//     default: //? delete soft
-//       return { isRead: !isRead }
-//       break;
-//   }
-// }
-
-
-//TODO : Building START
-
-let handleUpdateMulti = param => {
-  switch (param) {
-    case 'draft':
-      return { isRead: isRead, labels: 'personal' }
-      break;
-    case 'spam':
-      return { isRead: isRead, labels: 'company' }
-      break;
-    case 'trash':
-      return { isRead: isRead, labels: 'important' }
-      break;
-    case 'delete-soft':
-      return { isRead: isRead, labels: 'private' }
-    default:
-      return handleUpdateMultiLabels(param)
-  }
-}
-
-let handleUpdateMultiLabels = param => {
-  switch (param) {
-    case 'personal':
-      return { isRead: isRead, labels: 'personal' }
-      break;
-    case 'company':
-      return { isRead: isRead, labels: 'company' }
-      break;
-    case 'important':
-      return { isRead: isRead, labels: 'important' }
-      break;
-    case 'private':
-      return { isRead: isRead, labels: 'private' }
-  }
-}
-
-//TODO : Building END
 
 //! CODE API FOR PERMISSION SUPER ADMIN - ADMIN
 /* GET home Todo listing. */
@@ -230,28 +168,38 @@ router.patch('/task/detail/update/:id', async function (req, res, next) {
 });
 
 
-
 /* PATCH todo listing Update MultiRecord & count Record Update   . */
 // TODO: METHOD - PATCH
 // -u http://localhost:1509/todo/task/param=:options
 // ? Example : http://localhost:1509/mail/task/update-all/606f591f41340a452c5e8377
 router.patch('/task/update-all', async function (req, res, next) {
   try {
-    const _idArray = req.body._idArray;
+    const arr = [ObjectId("606f591f41340a452c5e8377"), ObjectId("606f591f41340a452c5e8378"), ObjectId("606f591f41340a452c5e8379")];
     const entry = await mailModel.updateMany(
       {
-        _id: { $in: _idArray }
+        _id:
+        {
+          $in: arr
+
+        }
       },
       {
-        $inc: {
-          isRead: false
+        $set: {
+          folder: req.body?.folder,
+          labels: req.body?.labels,
+          isRead: req.body?.isRead,
         }
-      })
+      },
+    )
+
+
+
     return res.status(200).json({
       success: true,
       data: entry
     });
   } catch (err) {
+    console.log(err)
     return res.status(500).json({
       success: false,
       error: 'Server Error'
