4a85db726358dbcbdd0caf541dbd86cc6355d79c
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-10 17:30:02 +0700

    Update email.js

--- routes/email.js
+++ routes/email.js
@@ -1,94 +1,83 @@
 const express = require('express');
 const router = express.Router();
-const todoModel = require('../model/schemaTodo');
+const mailModel = require('../model/schemaEMail');
 
-let isImportant = true;
-let isDeleted = true;
-let isCompleted = true;
+let isStarred = true;
+let isRead = true;
 
 let hasFilter = (task, keyword) => {
   switch (task) {
+    case 'sent':
+      return { isRead: isRead, title: keyword }
+      break;
+    case 'draft':
+      return { isRead: isRead, title: keyword }
+      break;
+    case 'starred':
+      return { isRead: isRead, isStarred: isStarred, title: keyword }
+      break;
+    case 'spam':
+      return { isRead: isRead, title: keyword }
+      break;
+    case 'trash':
+      return { isRead: isRead, title: keyword }
+      break;
+    case 'personal':
+      return { isRead: isRead, labels: 'medium', title: keyword }
+      break;
+    case 'company':
+      return { isRead: isRead, labels: 'company', title: keyword }
+      break;
     case 'important':
-      return { isImportant: isImportant, isDeleted: !isDeleted, title: keyword }
+      return { isRead: isRead, labels: 'important', title: keyword }
       break;
-    case 'completed':
-      return { isCompleted: isCompleted, isDeleted: !isDeleted, title: keyword }
-      break;
-    case 'deleted':
-      return { isDeleted: isDeleted, title: keyword }
-      break;
-    case 'low':
-      return { isDeleted: !isDeleted, tags: task, title: keyword }
-      break;
-    case 'update':
-      return { isDeleted: !isDeleted, tags: task, title: keyword }
-      break;
-    case 'medium':
-      return { isDeleted: !isDeleted, tags: 'medium', title: keyword }
-      break;
-    case 'high':
-      return { isDeleted: !isDeleted, tags: task, title: keyword }
-      break;
-    case 'team':
-      return { isDeleted: !isDeleted, tags: task, title: keyword }
+    case 'private':
+      return { isRead: isRead, labels: 'private', title: keyword }
       break;
     default:
-      return { isDeleted: !isDeleted, title: keyword }
-  }
-}
-
-const hasSort = type => {
-  switch (type) {
-    case 'title-asc':
-      return { title: 1 }
-      break;
-    case 'title-desc':
-      return { title: -1 }
-      break;
-    case 'due-date-desc':
-      return { dueDate: -1 }
-      break;
-    default:
-      return { dueDate: 1 }
+      return { isRead: !isRead, title: keyword }
   }
 }
 
 const hasTotalRecords = param => {
   switch (param) {
+    case 'sent':
+      return { folder: "sent", isRead: isRead }
+      break;
+    case 'draft':
+      return { folder: 'draft', isRead: isRead }
+      break;
+    case 'starred':
+      return { isStarred: isStarred, isRead: isRead }
+      break;
+    case 'spam':
+      return { folder: 'spam', isRead: isRead }
+      break;
+    case 'trash':
+      return { isRead: !isRead }
+      break;
+    case 'personal':
+      return { isRead: isRead, labels: 'personal' }
+      break;
+    case 'company':
+      return { isRead: isRead, labels: 'company' }
+      break;
     case 'important':
-      return { isImportant: isImportant }
+      return { isRead: isRead, labels: 'important' }
       break;
-    case 'completed':
-      return { isCompleted: isCompleted }
-      break;
-    case 'deleted':
-      return { isDeleted: isDeleted }
-      break;
-    case 'team':
-      return { tags: 'team', isDeleted: !isDeleted }
-      break;
-    case 'low':
-      return { tags: 'low', isDeleted: !isDeleted }
-      break;
-    case 'medium':
-      return { tags: 'medium', isDeleted: !isDeleted }
-      break;
-    case 'high':
-      return { tags: 'high', isDeleted: !isDeleted }
-      break;
-    case 'update':
-      return { tags: 'update', isDeleted: !isDeleted }
+    case 'private':
+      return { isRead: isRead, labels: 'private' }
       break;
 
     default:
-      return { isDeleted: !isDeleted }
+      return { isRead: isRead }
   }
 }
 
 //! CODE API FOR PERMISSION SUPER ADMIN - ADMIN
 /* GET home Todo listing. */
 // TODO: METHOD - GET
-// -u http://localhost:1509/todo/
+// -u http://localhost:1509/mail/
 router.get('/', async function (req, res, next) {
   res.send({
     status: 200,
@@ -98,11 +87,51 @@ router.get('/', async function (req, res, next) {
 
 /* GET todo listing view MyTask */
 // TODO: METHOD - GET
-// -u http://localhost:1509/todo/task/?query(filter=)&query(q=)&query(sort=)
-// ? Example : http://localhost:1509/todo/task?filter=important&sort=title-desc&page=1&perPage=10
+// -u http://localhost:1509/mail/task/?query(filter=)&query(q=)&query(sort=)
+// ? Example : http://localhost:1509/mail/task?labels=important&page=1&perPage=10
 router.get('/task', async function (req, res, next) {
   try {
-    let filter = req.query.filter;
+    let labels = req.query.labels;
+    let q = req.query.q;
+    let regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
+    console.log(labels, q)
+    //? Begin config Pagination
+    let pagination = {
+      currentPage: parseInt(req.query.page),
+      totalItemsPerPage: parseInt(req.query.perPage)
+    }
+
+    const taskOne = await mailModel.countDocuments(hasTotalRecords(labels));
+
+    const taskTwo = await mailModel
+      .find(hasFilter(labels, regex))
+      .limit(pagination.totalItemsPerPage)
+      .skip((pagination.currentPage - 1) * pagination.totalItemsPerPage);
+
+    Promise.all([taskOne, taskTwo]).then(([dataOne, dataTwo]) => {
+      return res.status(200).json({
+        success: true,
+        totalRecords: dataOne,
+        data: dataTwo,
+      });
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  };
+});
+
+/* GET todo listing return list todo follow Tags */
+
+// TODO: METHOD - GET
+// -u http://localhost:1509/mail/task/?query(tag=)&query(q=)&query(sort=)
+// ? Example : http://localhost:1509/mail/task/?tag=medium&q=c&sort=due-date-desc&page=1&perPage=10
+router.get('/task/tag', async function (req, res, next) {
+
+  try {
+    let tag = req.query.tag;
     let q = req.query.q;
     let regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
     let sort = req.query.sort;
@@ -112,10 +141,9 @@ router.get('/task', async function (req, res, next) {
       totalItemsPerPage: parseInt(req.query.perPage)
     }
 
-    const taskOne = await todoModel.countDocuments(hasTotalRecords(filter));
-
-    const taskTwo = await todoModel
-      .find(hasFilter(filter, regex))
+    const taskOne = await mailModel.countDocuments(hasTotalRecords(tag));
+    const taskTwo = await mailModel
+      .find(hasFilter(tag, regex))
       .sort(hasSort(sort))
       .limit(pagination.totalItemsPerPage)
       .skip((pagination.currentPage - 1) * pagination.totalItemsPerPage);
@@ -135,52 +163,14 @@ router.get('/task', async function (req, res, next) {
   };
 });
 
--/* GET todo listing return list todo follow Tags */
-
-  // TODO: METHOD - GET
-  // -u http://localhost:1509/todo/task/?query(tag=)&query(q=)&query(sort=)
-  // ? Example : http://localhost:1509/todo/task/?tag=medium&q=c&sort=due-date-desc&page=1&perPage=10
-  router.get('/task/tag', async function (req, res, next) {
-
-    try {
-      let tag = req.query.tag;
-      let q = req.query.q;
-      let regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
-      let sort = req.query.sort;
-      //? Begin config Pagination
-      let pagination = {
-        currentPage: parseInt(req.query.page),
-        totalItemsPerPage: parseInt(req.query.perPage)
-      }
-
-      const taskOne = await todoModel.countDocuments(hasTotalRecords(tag));
-      const taskTwo = await todoModel
-        .find(hasFilter(tag, regex))
-        .sort(hasSort(sort))
-        .limit(pagination.totalItemsPerPage)
-        .skip((pagination.currentPage - 1) * pagination.totalItemsPerPage);
-
-      Promise.all([taskOne, taskTwo]).then(([dataOne, dataTwo]) => {
-        return res.status(200).json({
-          success: true,
-          totalRecords: dataOne,
-          data: dataTwo,
-        });
-      })
-    } catch (err) {
-      return res.status(500).json({
-        success: false,
-        error: 'Server Error'
-      });
-    };
-  });
 /* GET Details users listing. */
 // TODO: METHOD - GET
-// -u http://localhost:1509/todo/task/detail/:id
+// -u http://localhost:1509/mail/task/detail/:id
+// ? Example: http://localhost:1509/mail/task/detail/606f591f41340a452c5e8376
 router.get('/task/detail/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
-    await todoModel
+    await mailModel
       .find({ _id: _id })
       .then(data => {
         return res.status(200).json({
@@ -196,74 +186,16 @@ router.get('/task/detail/:id', async function (req, res, next) {
   };
 });
 
-/* POST todo listing create a record. */
-// TODO: METHOD - POST
-// -u http://localhost:1509/todo/task/create
-router.patch('/task/create', async function (req, res, next) {
-  try {
-    const entry = await todoModel.create({
-      title: req.body?.title,
-      dueDate: req.body?.dueDate,
-      description: req.body?.description,
-      tags: req.body?.tags,
-      assignee: req.body?.assignee,
-      isCompleted: req.body?.isCompleted,
-      isImportant: req.body?.isImportant,
-      isDeleted: req.body?.isDeleted,
-      idAuthor: req.body?.idAuthor,
-    })
-    return res.status(200).json({
-      success: true,
-      data: entry
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      error: 'Server Error'
-    });
-  };
-
-});
-
-/* PUT todo listing. update an record */
-// TODO: METHOD - PUT
-// -u http://localhost:1509/todo/task/update/:id
-router.put('/task/update/:id', async function (req, res, next) {
-  try {
-    const _id = req.params.id;
-    const entry = await todoModel
-      .findByIdAndUpdate({ _id: _id }, {
-        title: req.body?.title,
-        dueDate: req.body?.dueDate,
-        description: req.body?.description,
-        tags: req.body?.tags,
-        assignee: req.body?.assignee,
-        isCompleted: req.body?.isCompleted,
-        isImportant: req.body?.isImportant,
-        isDeleted: req.body?.isDeleted,
-        idAuthor: req.body?.idAuthor,
-      })
-    return res.status(200).json({
-      success: true,
-      data: entry
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      error: 'Server Error'
-    });
-  };
-});
-
-
-/* PATCH todo listing change status isComplete. */
+/* PATCH todo listing change isStarred isComplete. */
 // TODO: METHOD - PATCH
-// -u http://localhost:1509/todo/task/change/:id
-router.patch('/task/change/:id', async function (req, res, next) {
+// -u http://localhost:1509/todo/task/is-starred/:id
+
+router.patch('/task/is-starred/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
-    const isCompleted = req.body?.isCompleted;
-    const entry = await todoModel.findByIdAndUpdate({ _id: _id }, { isCompleted: isCompleted });
+    const isStarred = req.body?.isStarred;
+
+    const entry = await todoModel.findByIdAndUpdate({ _id: _id }, { isStarred: isStarred });
     return res.status(200).json({
       success: true,
       data: entry
@@ -276,13 +208,14 @@ router.patch('/task/change/:id', async function (req, res, next) {
   };
 });
 
-/* PATCH todo listing deleteSoft Record */
-// TODO: METHOD - DELETE
+/* PATCH todo listing deleteSoft Record . */
+// TODO: METHOD - PATCH
 // -u http://localhost:1509/todo/task/delete-soft/:id
-router.delete('/task/delete-soft/:id', async function (req, res, next) {
+// ? Example : http://localhost:1509/mail/task/detail/delete-soft/606f591f41340a452c5e8377
+router.patch('/task/detail/delete-soft/:id', async function (req, res, next) {
   try {
     const _id = req.params.id;
-    const entry = await todoModel.updateOne({ _id: _id }, { isDeleted: true });
+    const entry = await mailModel.findByIdAndUpdate({ _id: _id }, { isRead: false });
     return res.status(200).json({
       success: true,
       data: entry
@@ -295,13 +228,14 @@ router.delete('/task/delete-soft/:id', async function (req, res, next) {
   };
 });
 
-/* DELETE todo listing deleteSoft Record */
 // TODO: METHOD - DELETE
-// -u http://localhost:1509/todo/task/delete/:id
+// -u http://localhost:1509/mail/task/delete/:id
+//? Example : http://localhost:1509/mail/task/delete/606f591f41340a452c5e8376
 router.delete('/task/delete/:id', async function (req, res, next) {
+
   try {
     const _id = req.params.id;
-    const entry = await todoModel.findByIdAndDelete({ _id: _id });
+    const entry = await mailModel.findByIdAndDelete({ _id: _id });
     return res.status(200).json({
       success: true,
       data: entry
@@ -313,18 +247,6 @@ router.delete('/task/delete/:id', async function (req, res, next) {
     });
   };
 });
-
-/* GET todo listing Search Record */
-// TODO: METHOD - GET
-// -u http://localhost:1509/todo/task/search/?keyword=abc
-
-// router.get('/task/search', async function (req, res, next) {
-//   let q = req.query.q;
-//   var regex = new RegExp(q, 'i');  // 'i' makes it case insensitive
-//   return todoModel.find({ title: regex }, function (err, q) {
-//     return res.send(q);
-//   });
-// });
 
 
 //! CODE API FOR PERMISSION EMPLOYEE
