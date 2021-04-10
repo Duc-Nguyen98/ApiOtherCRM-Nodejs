e2ad6974a6155f5cb3353096bbca63103cbd0d4e
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-10 16:11:45 +0700

    Create schemaEMail.js

---
+++ model/schemaEMail.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+const schema = new mongoose.Schema({
+
+    from: {
+        type: Object,
+        email: {
+            type: String,
+            default: ''
+        },
+        name: {
+            type: String,
+            default: ''
+        },
+        avatar: {
+            type: String,
+            default: ''
+        },
+    },
+    to: {
+        type: Array,
+        default: []
+    },
+    subject: {
+        type: String,
+        default: ''
+    },
+    cc: {
+        type: Array,
+        default: []
+    },
+    bcc: {
+        type: Array,
+        default: []
+    },
+    message: {
+        type: String,
+        default: ''
+    },
+    attachments: {
+        type: Array,
+        default: []
+    },
+
+    labels: {
+        type: Array,
+        default: []
+    },
+
+    isStarred: {
+        type: Boolean,
+        default: false
+    },
+
+    isRead: {
+        type: Boolean,
+        default: true
+    },
+
+    time: {
+        type: String,
+        default: ''
+    },
+    replies: {
+        type: Array,
+        default: []
+    },
+    folder: {
+        type: String,
+        default: ''
+    },
+});
+
+
+
+module.exports = mongoose.model('emails', schema);
