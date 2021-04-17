d791da9abd9ab70cf25a648cca8ffd1e87de068e
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-17 11:58:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var todoRouter = require('./routes/todo');
 var emailRouter = require('./routes/email');
 var customerRouter = require('./routes/customer');
+var userRouter = require('./routes/user');
 
 var app = express();
 
@@ -47,6 +48,7 @@ app.use('/', indexRouter);
 app.use('/todo', todoRouter);
 app.use('/mail', emailRouter);
 app.use('/customer', customerRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
