69a75ffa7b2e53a89a0af260a755d1d3f5a5470d
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-17 11:58:28 +0700

    Create schemaUser.js

---
+++ model/schemaUser.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+const schema = new mongoose.Schema({
+
+    avatar: {
+        type: String,
+        default: ''
+    },
+    name: {
+        type: String,
+        default: ''
+    },
+    gender: {
+        type: Number,
+        default: 0
+    },
+    birthDate: {
+        type: Date,
+        default: ''
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    email: {
+        type: String,
+        default: ''
+    },
+    account: {
+        type: String,
+        default: ''
+    },
+    password: {
+        type: String,
+        default: ''
+    },
+    created: {
+        type: Object,
+        CreateBy: {
+            type: String,
+            default: ''
+        },
+        time: {
+            type: Date,
+            default: ''
+        },
+    },
+    modified: {
+        type: Object,
+        modifiedBy: {
+            type: String,
+            default: ''
+        },
+        time: {
+            type: Date,
+            default: ''
+        },
+
+    },
+    softDelete: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('users', schema);
