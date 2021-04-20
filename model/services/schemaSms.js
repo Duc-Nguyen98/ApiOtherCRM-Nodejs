c7690e287cd7d59b0137c14b828de99aeb9cc269
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-21 01:45:17 +0700

    Create schemaSms.js

---
+++ model/services/schemaSms.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+const schema = new mongoose.Schema({
+    idServices: {
+        type: Number,
+        default: ''
+    },
+    title: {
+        type: String,
+        default: ''
+    },
+    type: {
+        type: Number,
+        default: ''
+    },
+    category: {
+        type: Number,
+        default: ''
+    },
+    name: {
+        type: String,
+        default: ''
+    },
+    status: {
+        type: Number,
+        default: 0
+    },
+    telephone: {
+        type: String,
+        default: ''
+    },
+    content: {
+        type: String,
+        default: ''
+    },
+    details: {
+        type: Object,
+        createBy: {
+            type: String,
+            default: ''
+        },
+        dateSent: {
+            type: String,
+            default: ''
+        }
+        , receivedDate: {
+            type: Date,
+            default: ''
+        },
+    },
+    softDelete: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('services_sms', schema);
