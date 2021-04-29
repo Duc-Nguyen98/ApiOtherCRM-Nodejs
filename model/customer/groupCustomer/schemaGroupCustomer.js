23f038b9c9fc68d83c1d9113bc5d49a23ed8e856
Nguyễn Quang Đức <52225998+Duc-Nguyen99@users.noreply.github.com>
2021-04-29 18:09:49 +0700

    Create schemaGroupCustomer.js

---
+++ model/customer/groupCustomer/schemaGroupCustomer.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+const schema = new mongoose.Schema({
+    idGroupCustomer: {
+        type: Number,
+        default: ''
+    },
+    avatarGroup: {
+        type: String,
+        default: ''
+    },
+    title: {
+        type: String,
+        default: ''
+    },
+    memberCustomer: {
+        type: Array,
+        default: []
+    },
+    status: {
+        type: Number,
+        default: 0
+    },
+    note: {
+        type: String,
+        default: 'ANT-CVV Note Shop'
+    },
+    star: {
+        type: Number,
+        default: 0
+    },
+
+    softDelete: {
+        type: Number,
+        default: 0
+    },
+
+    created: {
+        type: Object,
+        CreateBy: {
+            type: String,
+            default: ''
+        },
+        time: {
+            type: Date,
+            default: ''
+        }
+    },
+    modified: {
+        type: Object,
+        modifiedBy: {
+            type: String,
+            default: ''
+        },
+        time: {
+            type: Date,
+            default: ''
+        }
+    },
+
+
+});
+
+module.exports = mongoose.model('group_customers', schema);
