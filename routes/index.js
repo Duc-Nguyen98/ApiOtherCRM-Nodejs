62d25523f2c5f965f3051e825043ed4365f29831
Vũ Ngọc Tú <vungoctu.dev@gmail.com>
2021-05-08 18:17:06 +0700

    upodate

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,8 @@ const sgMail = require('@sendgrid/mail')
 const jwtConfig = {
   secret: 'dd5f3089-40c3-403d-af14-d0c228b05cb4',
   refreshTokenSecret: '7c4c1c50-3230-45bf-9eae-c9b2e401c767',
-  expireTime: '10m',
-  refreshTokenExpireTime: '10m',
+  expireTime: '7d',
+  refreshTokenExpireTime: '10d',
 }
 
 router.post('/login', async function (req, res, next) {
