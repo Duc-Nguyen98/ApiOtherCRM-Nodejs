4100abdde59a87092de38d47acafc9b7846355be
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-05-19 00:27:21 +0700

    Update homePage.js

--- routes/others/homePage.js
+++ routes/others/homePage.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const customerModel = require('../../model/customer/customer/schemaCustomer');
+const todoModel = require('../../model/schemaTodo');
 var moment = require('moment'); // require
 
 
+const changeStream = todoModel.watch(); // có thể là Mongo.watch() hoặc db.watch()
+changeStream.on('change', changeEvent => {
+  // process next document
+  console.log(changeEvent)
+});
 
-
-
+console.l
 
 router.get('/', async function (req, res, next) {
   try {
 
-    const currentDayAndMonth = moment(new Date()).format("MM-DD");
-    const customers = await customerModel.find({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
-    const countCustomer = await customerModel.countDocuments({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
-    return res.status(200).json({
-      success: true,
-      customers: customers,
-      countCustomerBirthDay: countCustomer
-    });
+    //? Birthday notifications
+    // const currentDayAndMonth = moment(new Date()).format("MM-DD");
+    // const customers = await customerModel.find({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
+    // const countCustomer = await customerModel.countDocuments({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
+    // return res.status(200).json({
+    //   success: true,
+    //   customers: customers,
+    //   countCustomerBirthDay: countCustomer
+    // });
 
   } catch (err) {
     console.log(err)
