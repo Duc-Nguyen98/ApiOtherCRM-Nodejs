22a47c8a27d41f60d53134c1947572b4b055a8f7
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-05-19 02:56:22 +0700

    update template Mail for forgotPassword

--- routes/others/homePage.js
+++ routes/others/homePage.js
@@ -4,27 +4,21 @@ const todoModel = require('../../model/schemaTodo');
 var moment = require('moment'); // require
 
 
-const changeStream = todoModel.watch(); // có thể là Mongo.watch() hoặc db.watch()
-changeStream.on('change', changeEvent => {
-  // process next document
-  console.log(changeEvent)
-});
 
-console.l
+
+
 
 router.get('/', async function (req, res, next) {
   try {
-
     //? Birthday notifications
-    // const currentDayAndMonth = moment(new Date()).format("MM-DD");
-    // const customers = await customerModel.find({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
-    // const countCustomer = await customerModel.countDocuments({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
-    // return res.status(200).json({
-    //   success: true,
-    //   customers: customers,
-    //   countCustomerBirthDay: countCustomer
-    // });
-
+    const currentDayAndMonth = moment(new Date()).format("MM-DD");
+    const customers = await customerModel.find({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
+    const countCustomer = await customerModel.countDocuments({ birthDay: { $regex: currentDayAndMonth, $options: "$i" }, softDelete: 0 });
+    return res.status(200).json({
+      success: true,
+      customers: customers,
+      countCustomerBirthDay: countCustomer
+    });
   } catch (err) {
     console.log(err)
     return res.status(500).json({
