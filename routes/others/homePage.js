1493f38c197f4694bb2227665a68abfe1a29330b
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-05-21 22:45:55 +0700

    Update homePage.js

--- routes/others/homePage.js
+++ routes/others/homePage.js
@@ -7,43 +7,9 @@ const customerModel = require('../../model/customer/customer/schemaCustomer');
 const servicesModel = require('../../model/schemaService');
 const usersModel = require('../../model/groupUser/schemaUser');
 
-
-
-const rankingGratitude = async (req, res, next) => {
-  try {
-    var rGratitude = [];
-    var rGratitude_old = [];
-    const entry = await servicesModel.aggregate(
-      [
-        { $match: { softDelete: 0 } },
-        {
-          $group: {
-            _id: "$idUser",
-            count: { $sum: 1 }
-          }
-        },
-        { $sort: { count: -1 } },
-        { $limit: 5 }
-      ]
-    ).then(data => {
-      rGratitude_old = data;
-    });
-    for (let i = 0; i < rGratitude_old.length; i++) {
-      let element2 = await usersModel.find({ idUser: rGratitude_old[i]._id }).select({ avatar: 1, name: 1, _id: 0 });
-      rGratitude.push({ idUser: rGratitude_old[i]._id, avatar: element2[0].avatar, name: element2[0].name, countGratitude: rGratitude_old[i].count });
-    }
-    rGratitude_new = rGratitude
-    next();
-  } catch (err) {
-    console.log(err)
-    return res.status(500).json({
-      success: false,
-      error: 'Server Error'
-    });
-  }
-}
-
-
+const thisMoment = moment();
+const endOfMonth = (moment().clone().startOf('month').format("X")) * 1000;
+const startOfMonth = (moment().clone().endOf('month').format("X")) * 1000;
 
 // router.get('/', async function (req, res, next) {
 //   try {
@@ -66,25 +32,126 @@ const rankingGratitude = async (req, res, next) => {
 // });
 
 
-router.get('/', checkAuthentication, rankingGratitude, async function (req, res, next) {
+router.get('/userWelcome', checkAuthentication, async function (req, res, next) {
   try {
-    let thisMoment = moment();
-    let endOfMonth = (moment().clone().startOf('month').format("X")) * 1000;
-    let startOfMonth = (moment().clone().endOf('month').format("X")) * 1000;
+    const userInformation = await usersModel.findOne({ idUser: userObj.idUser });
+    return res.status(200).json({
+      success: true,
+      userInformation: userInformation,
+    });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  };
+});
 
+router.get('/customerData', checkAuthentication, async function (req, res, next) {
+  try {
+    let totalCustomers = await customerModel.countDocuments({ softDelete: 0 });
+    let customersPerMonth = await customerModel.countDocuments({ "created.time": { $gte: startOfMonth, $lte: endOfMonth } })
 
-    // console.log(userObj)
-    const totalCustomers = await customerModel.countDocuments({ softDelete: 0 });
-    const customersPerMonth = await customerModel.countDocuments({ "created.time": { $gte: startOfMonth, $lte: endOfMonth } })
+    if (totalCustomers > 0 && totalCustomers < 10) {
+      totalCustomers = `0${totalCustomers}`;
+    } else {
+      totalCustomers = totalCustomers;
+    }
 
-    const totalServices = await servicesModel.countDocuments({ softDelete: 0 });
-    const servicesPerMonth = await servicesModel.countDocuments({ "details.time": { $gte: startOfMonth, $lte: endOfMonth } })
+    if (customersPerMonth > 0 && customersPerMonth < 10) {
+      customersPerMonth = `0${customersPerMonth}`;
+    } else {
+      customersPerMonth = customersPerMonth;
+    }
 
     return res.status(200).json({
       success: true,
       customerData: { totalCustomers: totalCustomers, customersPerMonth: customersPerMonth },
+    });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  };
+});
+
+router.get('/gratitudeCustomerData', checkAuthentication, async function (req, res, next) {
+  try {
+    const totalServices = await servicesModel.countDocuments({ softDelete: 0 });
+    const servicesPerMonth = await servicesModel.countDocuments({ "details.time": { $gte: startOfMonth, $lte: endOfMonth } });
+    if (totalServices > 0 && totalServices < 10) {
+      totalServices = `0${totalServices}`;
+    } else {
+      totalServices = totalServices;
+    }
+
+    if (servicesPerMonth > 0 && servicesPerMonth < 10) {
+      servicesPerMonth = `0${servicesPerMonth}`;
+    } else {
+      servicesPerMonth = servicesPerMonth;
+    }
+    return res.status(200).json({
+      success: true,
       gratitudeCustomerData: { totalGratitude: totalServices, gratitudePerMonth: servicesPerMonth },
-      rankingGratitude: rGratitude_new,
+    });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  };
+});
+
+
+router.get('/rankingGratitude', checkAuthentication, async function (req, res, next) {
+  try {
+    let rGratitude = [];
+    let rGratitude_old = [];
+    const entry = await servicesModel.aggregate(
+      [
+        { $match: { softDelete: 0 } },
+        {
+          $group: {
+            _id: "$idUser",
+            count: { $sum: 1 }
+          }
+        },
+        { $sort: { count: -1 } },
+        { $limit: 5 }
+      ]
+    ).then(data => {
+      rGratitude_old = data;
+    });
+    for (let i = 0; i < rGratitude_old.length; i++) {
+      let element2 = await usersModel.find({ idUser: rGratitude_old[i]._id }).select({ avatar: 1, name: 1, _id: 0 });
+      rGratitude.push({ idUser: rGratitude_old[i]._id, avatar: element2[0].avatar, name: element2[0].name, countGratitude: rGratitude_old[i].count });
+    }
+
+    return res.status(200).json({
+      success: true,
+
+      rankingGratitude: rGratitude,
+    });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  };
+});
+
+
+router.get('/tableServices', checkAuthentication, async function (req, res, next) {
+  try {
+    const servicesData = await servicesModel.find({ softDelete: 0 }).sort({ idUser: -1 })
+    return res.status(200).json({
+      success: true,
+      tableServices: servicesData
     });
   } catch (err) {
     console.log(err)
